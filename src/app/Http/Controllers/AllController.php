c2d0701d9ed99282267fd9f215d204a50a24a04e
NakagawaTakato <ntakato6231@gmail.com>
2024-03-23 19:50:37 +0900

    5

--- src/app/Http/Controllers/AllController.php
+++ src/app/Http/Controllers/AllController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 
 class AllController extends Controller
 {
-    public function register()
-    {
-        return view('register');
-    }
+    // public function register()
+    // {
+    //     return view('register');
+    // }
 
-    public function login()
-    {
-        return view('login');
-    }
+    // public function login()
+    // {
+    //     return view('login');
+    // }
 
     public function stamp()
     {
